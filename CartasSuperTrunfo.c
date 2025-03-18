3fdf7def13fdb327fca6629f203a50281cdef793
Sousa027 <200164069+Sousa027@users.noreply.github.com>
2025-03-18 01:15:30 +0000

    Duas cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,21 +1,76 @@
 #include <stdio.h>
 
 int main() {
-    char estado;
+    char estado[50];
     char codigo[20];
     char cidade[50];
     int populacao;
     float area;
     float pib;
     int turismo;
-;
+/// carta 1
 
+printf("digite o estado: \n");
+scanf("%s", estado);
+printf("Estado: %s\n", estado);
 
 
 printf("Digite o codigo da carta: \n");
 scanf("%s", codigo);
 printf("codigo da carta: %s\n", codigo);
     
+printf("digite a cidade: \n");
+scanf("%s", cidade);
+printf("cidade: %s\n", cidade);
+
+printf("digite a população: \n");
+scanf("%d", &populacao);
+printf("população: %d\n", populacao);
+
+printf("digite a area: \n");
+scanf("%f", &area);
+printf("area: %f\n", area);
+
+printf("Digite o pib: \n");
+scanf("%f", &pib);
+printf("Pib: %f\n", pib);
+
+printf("Digite a quatidade de pontos turisticos: \n");
+scanf("%d", &turismo);
+printf("Quantidade de pontos turísticos: %d\n", turismo);
+
+
+/// carta 2 
+
+printf("digite o estado: \n");
+scanf("%s", estado);
+printf("Estado: %s\n", estado);
+
+
+printf("Digite o codigo da carta: \n");
+scanf("%s", codigo);
+printf("codigo da carta: %s\n", codigo);
+    
+printf("digite a cidade: \n");
+scanf("%s", cidade);
+printf("cidade: %s\n", cidade);
+
+printf("digite a população: \n");
+scanf("%d", &populacao);
+printf("população: %d\n", populacao);
+
+printf("digite a area: \n");
+scanf("%f", &area);
+printf("area: %f\n", area);
+
+printf("Digite o pib: \n");
+scanf("%f", &pib);
+printf("Pib: %f\n", pib);
+
+printf("Digite a quatidade de pontos turisticos: \n");
+scanf("%d", &turismo);
+printf("Quantidade de pontos turísticos: %d\n", turismo);
+
 return 0;
     
 }
